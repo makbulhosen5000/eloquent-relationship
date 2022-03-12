acc03efe8f21f93486207d8bb0e4f264ef627484
mhakash5000 <mhakash5000@gmail.com>
2022-03-12 20:46:09 +0600

    added breeze athentication

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,10 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-center font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Eloquent Relationship') }}
+        </h2>
+    </x-slot>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -5,18 +12,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{-- font awesome cdn --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Eloquent Relationship</title>
   </head>
   <body>
 
        <div class="container">
            <div class="row">
                 <div class="col-sm-12">
-                    <h2 class="text-center my-3">Eloquent Relationship</h2>
                     <table class="table table-bordered text-center">
                         <thead>
                             <tr>
@@ -34,7 +38,6 @@
                                 <td>
                                     <a href="" class="btn btn-primary"><i class="fa fa-user-edit"></a>
                                     <a href="" class="btn btn-danger"><i class="fa fa-trash"></i></a>
-                                    <a href="" class="btn btn-success">View</a>
                                 </td>
                             </tbody>
                         </thead>
@@ -49,3 +52,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
+
+</x-app-layout>
